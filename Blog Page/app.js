c79d8def0de4759b9d621522280462a580fc7daa
Ananya <ananyaagrawal073@gmail.com>
2024-02-06 23:19:39 +0530

    commit
    commit

--- Blog Page/app.js
+++ Blog Page/app.js
@@ -1,40 +1,39 @@
 const express = require("express")
 const app = express()
-const path=require("path")
 const PORT = 4444;
-const hbs=require('hbs')
+const path=require("path")
+const hbs=require("hbs")
+const { v4: uuidv4 } = require('uuid');
 
-let blogarr=[]
-app.use(express.static(path.join(__dirname,"public")))
+let blogs=[]
+app.use(express.static(path.join(__dirname,'public')))
 app.use(express.urlencoded({extended:true}))
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
-
-app.post("/addblogs",(req,res)=>{
-    const {author,cateogry,blog}=req.body
-    const obj={
-        author,
-        cateogry,
-        blog
-    }
-    blogarr.push(obj)
-    res.send("data recieved")
-})
-
-// app.get("/getblogs",(req,res)=>{
-//     res.render('blogpage',{
-//         firstName:"Saksham",
-//         lastName:"Mishra"
-//     })
-// })
-
 app.get("/getblogs",(req,res)=>{
     res.render('blogpage',{
-     blogarr
+     blogs   
     })
 })
 
-app.listen(PORT, () => {
-    console.log("http://localhost:" + PORT);
+app.post("/addblog",(req,res)=>{
+    const {name,Class,blog}=req.body
+    const obj={
+        name,
+        Class,
+        blog,
+        blogId:uuidv4()
+    }
+    blogs.push(obj)
+    res.redirect("/getblogs")
+})
+
+app.get("/delete/:blogId",(req,res)=>{
+    blogs=blogs.filter((item)=>item.blogId!=req.params.blogId)
+    res.redirect("/getblogs")
+})
+
+app.listen(PORT,()=>{
+    console.log("http://localhost:"+PORT);
 })
