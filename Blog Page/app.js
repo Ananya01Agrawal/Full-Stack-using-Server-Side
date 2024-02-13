e58d9923ea48d15ed2f1cfba46db9add8d66e0ec
Ananya <ananyaagrawal073@gmail.com>
2024-02-13 08:24:14 +0530

    commit
    commit

--- Blog Page/app.js
+++ Blog Page/app.js
@@ -1,56 +1,56 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 const PORT = 4444;
-const path=require("path")
 const hbs=require("hbs")
 const { v4: uuidv4 } = require('uuid');
 
-let blogs=[]
-app.use(express.static(path.join(__dirname,'public')))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+
 app.set('view engine', 'hbs');
+
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.get("/getblogs",(req,res)=>{
-    res.render('blogpage',{
-     blogs   
-    })
-})
+let blogs=[]
 
 app.post("/addblog",(req,res)=>{
+    // console.log(req.body);
     const {name,Class,blog}=req.body
     const obj={
-        name,
-        Class,
-        blog,
+        name:name,
+        Class:Class,
+        blog:blog,
         blogId:uuidv4()
     }
+   console.log(obj);
     blogs.push(obj)
-    res.redirect("/getblogs")
+    // res.send(blogs)
+    res.redirect('/getblog')
+})
+
+
+
+app.get("/getblog",(req,res)=>{
+    res.render("blogpage",{
+        blogs:blogs
+    })
 })
 
 app.get("/delete/:blogId",(req,res)=>{
+    // console.log(req.params);
     blogs=blogs.filter((item)=>item.blogId!=req.params.blogId)
-    res.redirect("/getblogs")
+    res.redirect("/getblog")
 })
 
-app.get("/update/:blogId", (req, res) => {
-    console.log(req.params);
-    const updateblog = blogs.filter((item) => item.blogId == req.params.blogId)
-    console.log(updateblog);
-    res.render('updateblog', {
-        updateblog:updateblog[0]
-    })
+app.get("/update/:blogId",(req,res)=>{
+  const updateblog=blogs.filter((item)=>item.blogId==req.params.blogId)
+//   console.log(updateblog);
+  res.render("updateblog",{
+    updateblog:updateblog[0]
+  })
 })
-app.post("/updateblog", (req, res) => {
-    const { name, Class, blog, blogId } = req.body
-    const newObj = {
-        name: name,
-        Class: Class,
-        blog: blog,
-        blogId: blogId
-    }
 
-app.listen(PORT,()=>{
-    console.log("http://localhost:"+PORT);
+app.listen(PORT, () => {
+    console.log("http://localhost:" + PORT);
 })
